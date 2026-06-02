2c2c7ab94f20f60d40dd7b9e96fcdfa9f6f7c72e
pedrinzz10 <opedro485@gmail.com>
2026-06-02 14:41:33 -0300

    Projeto inicial gerado no spring io

---
+++ src/main/java/br/com/fiap/satmonitor/SatmonitorApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.satmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatmonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatmonitorApplication.class, args);
+	}
+
+}
