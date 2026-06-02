cfd73f204cd832d70ef2326dc80f92bf2553c714
pedrinzz10 <opedro485@gmail.com>
2026-06-02 15:32:27 -0300

    feat: cria SenhaMissaoInvalidaException (401) para senha incorreta ao entrar em missão

---
+++ src/main/java/br/com/fiap/satmonitor/exception/SenhaMissaoInvalidaException.java
@@ -0,0 +1,7 @@
+package br.com.fiap.satmonitor.exception;
+
+public class SenhaMissaoInvalidaException extends RuntimeException {
+    public SenhaMissaoInvalidaException(String message) {
+        super(message);
+    }
+}
