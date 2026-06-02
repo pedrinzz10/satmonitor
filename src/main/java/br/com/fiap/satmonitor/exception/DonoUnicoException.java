a4f2c54804e36e47b02ae0edf29cfd88180595dc
pedrinzz10 <opedro485@gmail.com>
2026-06-02 15:32:36 -0300

    feat: cria DonoUnicoException (400) para único dono que tenta sair sem transferir propriedade

---
+++ src/main/java/br/com/fiap/satmonitor/exception/DonoUnicoException.java
@@ -0,0 +1,7 @@
+package br.com.fiap.satmonitor.exception;
+
+public class DonoUnicoException extends RuntimeException {
+    public DonoUnicoException(String message) {
+        super(message);
+    }
+}
