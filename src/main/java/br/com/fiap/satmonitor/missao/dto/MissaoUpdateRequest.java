080144f7adc0e28cfca5b8bb3a124c78cfbafa9d
pedrinzz10 <opedro485@gmail.com>
2026-06-02 21:02:01 -0300

    feat: cria record MissaoUpdateRequest separado de MissaoRequest para não permitir atualização de senha via PUT

---
+++ src/main/java/br/com/fiap/satmonitor/missao/dto/MissaoUpdateRequest.java
@@ -0,0 +1,14 @@
+package br.com.fiap.satmonitor.missao.dto;
+
+import br.com.fiap.satmonitor.missao.enums.StatusMissao;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+
+public record MissaoUpdateRequest(
+        @NotBlank String nome,
+        String descricao,
+        @NotNull LocalDate dataLancamento,
+        @NotNull StatusMissao status
+) {}
