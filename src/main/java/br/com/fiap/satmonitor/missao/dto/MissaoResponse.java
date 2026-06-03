56a99fb8a44af8500bb559a2b27b35f69ffd629b
pedrinzz10 <opedro485@gmail.com>
2026-06-02 21:02:10 -0300

    feat: cria MissaoResponse com roleDoOperador, totalMembros e totalSatelites estendendo RepresentationModel

---
+++ src/main/java/br/com/fiap/satmonitor/missao/dto/MissaoResponse.java
@@ -0,0 +1,25 @@
+package br.com.fiap.satmonitor.missao.dto;
+
+import br.com.fiap.satmonitor.missao.enums.RoleMissao;
+import br.com.fiap.satmonitor.missao.enums.StatusMissao;
+import lombok.*;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MissaoResponse extends RepresentationModel<MissaoResponse> {
+
+    private Long id;
+    private String nome;
+    private String descricao;
+    private LocalDate dataLancamento;
+    private StatusMissao status;
+    private String roleDoOperador;
+    private Integer totalMembros;
+    private Integer totalSatelites;
+}
