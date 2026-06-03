892634cc3c1f73ab4adbb35a1f6acbf0fdcb7e87
pedrinzz10 <opedro485@gmail.com>
2026-06-02 21:01:47 -0300

    feat: cria entidade Missao com tabela TB_MISSAO, sequence SEQ_MISSAO e senha armazenada em BCrypt

---
+++ src/main/java/br/com/fiap/satmonitor/missao/entity/Missao.java
@@ -0,0 +1,54 @@
+package br.com.fiap.satmonitor.missao.entity;
+
+import br.com.fiap.satmonitor.auth.entity.Operador;
+import br.com.fiap.satmonitor.missao.enums.StatusMissao;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "TB_MISSAO")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Missao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_missao")
+    @SequenceGenerator(name = "seq_missao", sequenceName = "SEQ_MISSAO", allocationSize = 1)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(columnDefinition = "CLOB")
+    private String descricao;
+
+    @NotNull
+    @Column(nullable = false)
+    private LocalDate dataLancamento;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private StatusMissao status;
+
+    @Column(nullable = false)
+    private String senhaMissao;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "operador_dono_id")
+    private Operador operadorDono;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "missao", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OperadorMissao> membros = new ArrayList<>();
+}
